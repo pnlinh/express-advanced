f09e41a1f41d46c9f27854fbf510e936267149bb
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-21 23:56:32 +0700

    create custom middleware

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const Joi = require('joi');
 const express = require('express');
 const app = express();
+const logger = require('./logger');
 
 app.use(express.json());
 
+app.use(logger);
+
 const port = process.env.PORT || 3000;
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+    {id: 4, name: 'course4'}
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello NodeJs');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
