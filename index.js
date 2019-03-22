4f12542ad38258ef44238485d82a4f7afec4848b
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-22 09:40:24 +0700

    template engine

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const config = require('config');
 const Joi = require('joi');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
-const logger = require('./logger');
+// Set views
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-
 app.use(helmet());
 
 console.log(`Application name: ${config.get('name')}`);
@@ -22,57 +24,12 @@ if (app.get('env') === 'development') {
     console.log('Morgan enabled...');
 }
 
-app.use(logger);
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My Express App',
+        message: 'Hello NodeJs'
+    });
+});
 
 const port = process.env.PORT || 3000;
-
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-    {id: 4, name: 'course4'}
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello NodeJs');
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-    // Handle validate input
-    const {error} = validateCourse(req.body); // What is error ? Must be dump result validator
-
-    if (error) {
-        return res.status(400).send({
-            error: error.details[0].message
-        });
-    }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-
-    courses.push(course);
-
-    res.send(course);
-});
-
-/**
- * Handle validate input
- *
- * @param course
- * @returns {ValidationResult<*>}
- */
-function validateCourse(course) {
-    const rules = {
-        name: Joi.string().min(3).required()
-    };
-
-    return Joi.validate(course, rules);
-}
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
